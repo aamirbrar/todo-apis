874d072677c8c766b3ea0960dda3d8e6f28a9d8a
Aamir brar <amir@apporio.com>
2018-01-30 19:22:28 +0530

    Add get /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos',(req,res) =>{
     });
 });
 
+app.get('/todos',(req,res) =>{
+    Todo.find().then((todos) => {
+        res.status(200).send({todos});
+    },(e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000,() => {
     console.log('Start on Port 3000');
 });
