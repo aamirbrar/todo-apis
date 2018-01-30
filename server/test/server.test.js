bfad7254fa56277b7793a27a6d97bb11143988c9
Aamir brar <amir@apporio.com>
2018-01-30 19:35:18 +0530

    Add tests for GET/todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,8 +5,15 @@ const {app} = require('./../server');
 
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text:'first'},{
+    text:'second' }
+   ];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 describe('POST /todos',() => {
     it('should create a new todo',(done) => {
@@ -23,7 +30,7 @@ describe('POST /todos',() => {
                 if(err){
                     return done(err);
                 }
-            Todo.find().then((todos) =>{
+            Todo.find({text}).then((todos) =>{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -41,9 +48,22 @@ describe('POST /todos',() => {
                 return done(err);
             }
         Todo.find().then((todos) =>{
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
         }).catch((e) => done(e));
         });
     });
+});
+
+
+describe('GET /todos',() => {
+    it('should get all todos',(done) =>{
+        request(app)
+          .get('/todos')
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.todos.length).toBe(2);
+          })
+          .end(done);
+    } )
 });
