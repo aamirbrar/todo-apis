d3e3778910bd57baa9c7eae7b8cfa9016d255f45
Aamir brar <amir@apporio.com>
2018-01-30 18:37:26 +0530

    Add Post Todo route and Mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo',{
+    text:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:3
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    },
+    completedAt:{
+        type:Number,
+        default:null
+    }
+});
+module.exports = {Todo};
