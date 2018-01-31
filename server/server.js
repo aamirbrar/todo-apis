48f7df241598a0479bcd13e4658859a069f63716
Aamir brar <amir@apporio.com>
2018-01-31 17:49:05 +0530

    Add Delete/Todos/:id

--- server/server.js
+++ server/server.js
@@ -28,6 +28,20 @@ app.get('/todos',(req,res) =>{
     });
 });
 
+app.delete('/todos/:id',(req,res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send({msg:'Invalid Id'});
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+         if(!todo){
+            return res.status(400).send({msg:'No Todo Found'});
+        }
+        res.status(200).send({todo});
+    },(e) => {
+        res.status(400).send(e);
+    });
+});
 app.get('/todos/:id',(req,res) => {
     var id = req.params.id;
     if(!ObjectID.isValid(id)){
