08f35c25fad60f463544db859d1ccd2d67583cb7
Aamir brar <amir@apporio.com>
2018-02-02 16:54:04 +0530

    hash user password Before Save

--- playgroud/hashing.js
+++ playgroud/hashing.js
@@ -1,12 +1,26 @@
-const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
-var data = {
- id:10
-}
-var token = jwt.sign(data,'1con23Aamir');
-console.log(token);
-var decode = jwt.verify(token,'1con23Aamir');
-console.log('decoded:',decode);
+const bcrypt = require('bcryptjs');
+
+var password = "123ab!";
+
+// bcrypt.genSalt(10,(err,salt) => {
+//   bcrypt.hash(password,salt,(err,hash) => {
+//       console.log(hash);
+//   });
+// });
+
+var hashPassword = "$2a$10$KWfPbUmWqD1Zi.ncBYKwLeceh6GW/GSbnhhGGZ5eMNgdPAHm34mU6";
+bcrypt.compare(password,hashPassword,(err,res) => {
+    console.log(res);
+});
+// const {SHA256} = require('crypto-js');
+// const jwt = require('jsonwebtoken');
+// var data = {
+//  id:10
+// }
+// var token = jwt.sign(data,'1con23Aamir');
+// console.log(token);
+// var decode = jwt.verify(token,'1con23Aamir');
+// console.log('decoded:',decode);
 //jwt.verify
 // var msg = "I am user number 3";
 // var hash = SHA256(msg).toString();
