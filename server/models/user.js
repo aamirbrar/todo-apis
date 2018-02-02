b1be37ba8fa2b0c06875a51115af9999fec8c9e3
Aamir brar <amir@apporio.com>
2018-02-02 19:33:04 +0530

    Add user POST login

--- server/models/user.js
+++ server/models/user.js
@@ -76,6 +76,23 @@ UserSchema.pre('save',function(next){
         next();
     }
 });
+UserSchema.statics.finByCredentails = function(email,password){
+    var User = this;
+    return User.findOne({email}).then((user) => {
+        if(!user){
+           return Promise.reject();
+        }
+        return new Promise((resolve,reject) => {
+          bcrypt.compare(password,user.password,(err,res) => {
+              if(res){
+                  resolve(user);
+              } else {
+                  reject(err);
+              }
+          });
+        });
+    })
+};
 var User = mongoose.model('User',UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -13,7 +13,18 @@ var app = express();
 const port = process.env.PORT;
 app.use(bodyParser.json());
 
+//post login
+app.post('/users/login',(req,res) => {
+    var body = _.pick(req.body,['email','password']);
 
+    User.finByCredentails(body.email,body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 //get user with token
 app.get('/users/me',authenticate,(req,res) => {
     res.send(req.user);
